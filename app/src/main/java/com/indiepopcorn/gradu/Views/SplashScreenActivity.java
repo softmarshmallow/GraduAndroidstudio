6dfe85ef679521820650c26d3c616a6911fffd95
woojoo <woojoo@softmarshmallow.com>
2018-02-10 17:51:37 +0900

    2

---
+++ app/src/main/java/com/indiepopcorn/gradu/Views/SplashScreenActivity.java
@@ -0,0 +1,16 @@
+package com.indiepopcorn.gradu.Views;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.indiepopcorn.gradu.R;
+
+public class SplashScreenActivity extends AppCompatActivity
+{
+        
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+                super.onCreate(savedInstanceState);
+                setContentView(R.layout.activity_splash_screen);
+        }
+}
